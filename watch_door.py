1c783815e016f3991145ec70dca363e8f21e09e0
Unknown <koosc2@gmail.com>
2018-12-04 00:51:45 -0800

    Catch any exceptions to prevent geting stuck

--- watch_door.py
+++ watch_door.py
@@ -32,43 +32,48 @@ august.activity.epoch_to_datetime = epoch_to_datetime
 def check_door(token):
     global last_alert_time
 
-    # Get house assuming account only has 1 house
-    house = api.get_houses(token)[0]
+    try:
 
-    # Get lock ID assuming only 1 lock
-    lock = api.get_locks(token)[0]
-    lock_id = lock.device_id
+        # Get house assuming account only has 1 house
+        house = api.get_houses(token)[0]
 
-    activities = api.get_house_activities(token, house['HouseID'])
+        # Get lock ID assuming only 1 lock
+        lock = api.get_locks(token)[0]
+        lock_id = lock.device_id
 
-    last_activity = activities[0]
-    last_time = last_activity.activity_end_time / 1000 # convert to seconds
-    last_action = last_activity.action
+        activities = api.get_house_activities(token, house['HouseID'])
 
-    current_time = datetime.now().timestamp()
-    lock_status = api.get_lock_status(token, lock.device_id)
+        last_activity = activities[0]
+        last_time = last_activity.activity_end_time / 1000 # convert to seconds
+        last_action = last_activity.action
 
-    time_diff = (current_time - last_time)
+        current_time = datetime.now().timestamp()
+        lock_status = api.get_lock_status(token, lock.device_id)
 
-    # Check if door has been unlocked for too long
-    if time_diff > (ALERT_THRESHOLD) and last_action == 'unlock' and str(lock_status) == 'LockStatus.UNLOCKED':
-        try:
-            if (current_time - last_alert_time) > ALERT_COOLDOWN:
-                logger.info(f'door left unlocked for {time_diff} seconds. sending alert')
-                response = requests.post(ALERT_ENDPOINT, timeout=10)
-                response.raise_for_status()
+        time_diff = (current_time - last_time)
 
-                last_alert_time = current_time
-            else:
-                logger.info(f'no alert sent. {current_time - last_alert_time} seconds since last alert')
-        except requests.exceptions.RequestException:
-            logger.exception('exception posting alert')
-            
-        if AUTO_LOCK and time_diff > AUTO_LOCK_THRESHOLD:
-            logger.warning('auto locking door')
-            api.lock(token, lock_id)
-    else:
-        logger.debug(f'lock status is {lock_status} for {time_diff} seconds')
+        # Check if door has been unlocked for too long
+        if time_diff > (ALERT_THRESHOLD) and last_action == 'unlock' and str(lock_status) == 'LockStatus.UNLOCKED':
+            try:
+                if (current_time - last_alert_time) > ALERT_COOLDOWN:
+                    logger.info(f'door left unlocked for {time_diff} seconds. sending alert')
+                    response = requests.post(ALERT_ENDPOINT, timeout=10)
+                    response.raise_for_status()
+
+                    last_alert_time = current_time
+                else:
+                    logger.info(f'no alert sent. {current_time - last_alert_time} seconds since last alert')
+            except requests.exceptions.RequestException:
+                logger.exception('exception posting alert')
+                
+            if AUTO_LOCK and time_diff > AUTO_LOCK_THRESHOLD:
+                logger.warning('auto locking door')
+                api.lock(token, lock_id)
+        else:
+            logger.debug(f'lock status is {lock_status} for {time_diff} seconds')
+
+    except Exception:
+        logger.exception('exception encountered')
 
 if __name__ == '__main__':
 
